dd1d35806322e67431f1d2404b302251390d3575
r <r.thomas8@lancaster.ac.uk>
2024-06-29 00:38:45 +0100

    Replaced SystemTime with time::Instant, systemtime not monotonic

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 use std::{
-    error::Error,
-    net::UdpSocket,
-    time::{Duration, SystemTime},
+    error::Error, net::UdpSocket, process, time::{Duration, Instant}
 };
 
 const TELLO_IP: &'static str = "0.0.0.0:8889";
@@ -13,6 +11,7 @@ pub struct Drone {
     is_flying: bool,
     stream_on: bool,
     retry_count: i32,
+    last_command_time: Instant,
 }
 
 impl Drone {
@@ -30,6 +29,7 @@ impl Drone {
             is_flying: false,
             stream_on: false,
             retry_count: 3,
+            last_command_time: Instant::now(),
         }
     }
 
@@ -46,14 +46,19 @@ impl Drone {
 
     }
 
-    fn send_command_with_return(&self, command: &str, timeout: u64) -> &'static str {
+    // Some placeholder code, need to rewrite this
+    fn send_command_with_return(&mut self, command: &str, timeout: u64) -> &'static str {
+        let time_since_last_command = Instant::now().duration_since(self.last_command_time);
 
+        println!("{:?}", time_since_last_command);
+
+        self.last_command_time = Instant::now();
         "placeholder"
     }
 
     // Sends control command to Tello and waits for a response
-    fn send_control_command(&self, command: &str, timeout: u64) -> Result<(), Box<dyn Error>> {
-        for i in 0..self.retry_count {
+    fn send_control_command(&mut self, command: &str, timeout: u64) -> Result<(), Box<dyn Error>> {
+        for _ in 0..self.retry_count {
             let response: &str = self.send_command_with_return(command, timeout);
 
             if response.to_lowercase().contains("ok") {
@@ -61,9 +66,29 @@ impl Drone {
             }
         }
 
-        Ok(())
+        Err("Command {command} failed to run".into())
     }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use std::time;
+    use std::thread;
+
+    use super::*;
+
+    #[test]
+    fn system_time_test() {
+        let socket = UdpSocket::bind(TELLO_IP).expect("couldn't bind to address");
+
+        let mut d = Drone {
+            socket,
+            is_flying: false,
+            stream_on: false,
+            retry_count: 3,
+            last_command_time: Instant::now(),
+        };
+
+        d.send_command_with_return("a", 6);
+    }
+}
